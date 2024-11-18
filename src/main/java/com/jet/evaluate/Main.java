f26778d3f6a6f3a51c1502d6536e86c05965bc81
annaivanova <ivanoanni@gmail.com>
2024-11-18 18:47:57 +0100

    protected evaluate check

--- src/main/java/com/jet/evaluate/Main.java
+++ src/main/java/com/jet/evaluate/Main.java
@@ -1,8 +1,24 @@
 package com.jet.evaluate;
 
 
+import java.util.function.Supplier;
+
 public class Main {
+    public static <T> T wrap(Supplier<T> supplier) {
+        return supplier.get();
+    }
+
+    public static class Subclass {
+    }
+
+    public static Object[] create() {
+        return new Subclass[]{};
+    }
+
+
     public static void main(String[] args) {
+
+        wrap(() -> create());
 
         Evaluate evaluate = new Evaluate();
         evaluate.getName();
