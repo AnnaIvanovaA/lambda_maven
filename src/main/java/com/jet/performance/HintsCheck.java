18b77bba11861f4afd8eedc89eb0e2e1f5212718
Anna.Ivanova <anna.ivanova@jetbrains.com>
2024-12-25 16:06:41 +0100

    Add HintsCheck class for performance testing

---
+++ src/main/java/com/jet/performance/HintsCheck.java
@@ -0,0 +1,33 @@
+package com.jet.performance;
+
+public class HintsCheck {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println();
+        Thread.sleep(100);
+        Thread.sleep(1000);
+        Thread.sleep(1000);
+        Thread.sleep(100);
+        System.out.println();
+
+        B.test();
+        B.test2();
+        B.test();
+        B.test2();
+        System.out.println();
+    }
+
+    class B {
+        static void test(){
+            for (int i = 0; i < 10000; i++) {
+                System.out.println(i);
+            }
+        }
+        static void test2(){
+            for (int i = 0; i < 100000; i++) {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
